7aae534daa476765bd269ac8ada2d647f6b65d00
MaxUlianov <curze.curze22@gmail.com>
2022-12-03 10:57:10 +0100

    Added celery, edited tasks

--- messageManager/manager/models.py
+++ messageManager/manager/models.py
@@ -6,11 +6,12 @@ class Mailing(models.Model):
     id = models.AutoField(primary_key=True)
     start_time = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
     text = models.CharField(max_length=500)
-    filter = models.CharField(max_length=50)
+    filter_mobile_code = models.CharField(max_length=3, blank=True, default='')
+    filter_tag = models.CharField(max_length=20, blank=True, default='')
     end_time = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
 
     def __str__(self):
-        return f'{self.id}, {self.text[:15]}, {self.filter}'
+        return f'{self.id}, {self.text[:15]}, {self.filter_mobile_code}, {self.filter_tag}'
 
 
 class Client(models.Model):
@@ -19,8 +20,8 @@ class Client(models.Model):
     id = models.AutoField(primary_key=True)
     phone = models.CharField(max_length=11)
     mobile_code = models.CharField(max_length=3)
-    tag = models.CharField(max_length=50)
-    timezone = models.CharField(max_length=50, choices=TIMEZONES, blank=True, null=True, )
+    tag = models.CharField(max_length=20)
+    timezone = models.CharField(max_length=50, choices=TIMEZONES, blank=True, null=True)
 
     def __str__(self):
         return f'{self.id}, {self.phone}, {self.mobile_code}, {self.tag}'
--- messageManager/manager/tasks.py
+++ messageManager/manager/tasks.py
@@ -1,8 +1,52 @@
 from .serializers import MessageSerializer, ClientSerializer, MailingSerializer
 from .models import Message, Client, Mailing
+from celery import shared_task
+import requests
+from messageManager.settings import TOKEN
 
 
+def message_request():
+    token = TOKEN
+    msgid = 1
+
+    url = f'https://probe.fbrq.cloud/v1/send/{msgid}'
+
+    data = {
+        "id": msgid,
+        "phone": 0,
+        "text": "string"
+    }
+
+    headers = {
+        'accept': 'application/json',
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+
+    res = requests.post(url, headers=headers, json=data)
+    # res_m = res.json()['message']
+
+
+@shared_task()
 def send_message(mail_id):
     mailing = Mailing.objects.filter(id=mail_id)
-    client_filter = mailing.filter
+    client_filter_tag = mailing.filter_tag
+    client_filter_code = mailing.filter_mobile_code
 
+    if client_filter_tag != '':
+        client_list = Client.object.filter(tag=client_filter_tag)
+    elif client_filter_code != '':
+        client_list = Client.object.filter(mobile_code=client_filter_code)
+    else:
+        client_list = Client.objects.all()
+
+    for client in client_list:
+        print(client)
+        pass
+
+    # for clients
+    # make request
+    # whatever data comes, save as message object
+    # next one
+
+    url = 'https://probe.fbrq.cloud/docs'
